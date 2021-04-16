64c3177d2877f82c5c2d9ef55758796eb0102e55
diggerb <james@briant.co.uk>
2021-04-16 14:24:08 +0100

    Update PDEFunctions.R

--- src/include/PDEFunctions.R
+++ src/include/PDEFunctions.R
@@ -8,8 +8,8 @@ StepFunction2D <- function(x, y){
   output <- numeric(n)
   
   for(i in 1:n){
-    if(x[i] > centre.x[1] && x[i] < centre.x[2]){
-      if(y[i] > centre.y[1] && y[i] < centre.y[2]){
+    if(x[i] >= centre.x[1] && x[i] <= centre.x[2]){
+      if(y[i] >= centre.y[1] && y[i] <= centre.y[2]){
         output[i] <- 1
       }else{
         output[i] <- 0
