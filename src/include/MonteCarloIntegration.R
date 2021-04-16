c3903d7287594c5bbe9d29a968cd393d51eda722
diggerb <james@briant.co.uk>
2021-04-16 14:23:53 +0100

    Update MonteCarloIntegration.R

--- src/include/MonteCarloIntegration.R
+++ src/include/MonteCarloIntegration.R
@@ -1,9 +1,9 @@
-MonteCarlo2D <- function(user.func, sample=FALSE, a0=0, a1=1, b0=0, b1=1){
+MonteCarlo2D <- function(user.func, sample=FALSE, a0=0, a1=1, b0=0, b1=1, N=5000){
   # Assumes domain is [0,1]x[0,1]
   # domain is rectangular
   
   if(identical(sample, FALSE)){
-    sample = Generate2DUniformSample(N=5000, a0, a1, b0, b1)
+    sample = Generate2DUniformSample(N=N, a0, a1, b0, b1)
   }
   
   data <- user.func(sample$x, sample$y)
